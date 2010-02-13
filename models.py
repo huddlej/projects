1b9fe6172f08582ed1f0c61484adb651428767c4
John Huddleston <huddlej@gmail.com>
2010-02-13 14:30:10 -0800

    renamed datetime fields, added priority field

--- models.py
+++ models.py
@@ -9,6 +9,9 @@ class Task(models.Model):
     Represents a specific task to be completed for a specific milestone by one
     or more people.
     """
+    PRIORITY_CHOICES = (("a", "a"),
+                        ("b", "b"),
+                        ("c", "c"))
     STATUS_CHOICES = (("open", "Open"),
                       ("closed", "Closed"),
                       ("wontfix", "Won't Fix"),
@@ -16,12 +19,13 @@ class Task(models.Model):
 
     name = models.CharField(max_length=255)
     description = models.TextField(null=True, blank=True)
-    added_date = models.DateTimeField(editable=False)
-    closed_date = models.DateTimeField(editable=False, null=True, blank=True)
-    reported_by = models.ForeignKey(User,
-                                    related_name="reported_tasks")
+    priority = models.CharField(max_length=1, choices=PRIORITY_CHOICES)
+    created_on = models.DateTimeField(editable=False)
+    closed_on = models.DateTimeField(editable=False, null=True, blank=True)
     assigned_to = models.ManyToManyField(User,
                                          limit_choices_to={"is_staff": True})
+    reported_by = models.ForeignKey(User,
+                                    related_name="reported_tasks")
     status = models.CharField(max_length=255,
                               choices=STATUS_CHOICES,
                               default="open")
@@ -29,15 +33,11 @@ class Task(models.Model):
     def __unicode__(self):
         return self.name
 
-    @models.permalink
-    def get_absolute_url(self):
-        return ("projects_task", (self.id,))
-
     def save(self, *args, **kwargs):
         if not self.id:
-            self.added_date = datetime.datetime.now()
+            self.created_on = datetime.datetime.now()
         
         if self.status == "closed":
-            self.closed_date = datetime.datetime.now()
+            self.closed_on = datetime.datetime.now()
 
         super(Task, self).save(*args, **kwargs)
