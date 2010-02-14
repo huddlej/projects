bfb52feef0e0837493c210566f12ee71d14c3bae
John Huddleston <huddlej@gmail.com>
2010-02-13 20:18:30 -0800

    removed old views and created a simple index view with an index template

--- views.py
+++ views.py
@@ -1,97 +1,30 @@
 """
 Views for Projects application.
 """
-from django.core.urlresolvers import reverse
-from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import get_object_or_404, render_to_response
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render_to_response
 from django.template import RequestContext
-from django.utils import simplejson
-from django.views.generic.create_update import create_object
-from django.views.generic.list_detail import object_list
 
-from forms import MilestoneForm, TasksForm
-from models import Milestone, Project
+from forms import TaskForm
+from models import Task
 
 
-class JsonResponse(HttpResponse):
-    """Represents a Django response containing JSON data."""
-    def __init__(self, data):
-        super(JsonResponse, self).__init__(
-            content=simplejson.dumps(data),
-            mimetype="application/json"
-        )
+@login_required
+def index(request):
+    tasks = Task.objects.all()
+    post_data = request.POST or None
+    form = TaskForm(post_data)
 
-
-def validate_form(request, form_class):
-    """Validates a given form class when posted to by an AJAX call."""
-    form = form_class(request.POST)
-    if request.GET.has_key("field"):
-        errors = form.errors.get(request.GET["field"], [])
-    else:
-        errors = form.errors
-
-    return JsonResponse({
-        "valid": not errors,
-        "errors": errors
-    })
-
-
-def projects(request):
-    """Represents a list of all projects."""
-    form_response = create_object(request, model=Project)
-    if not form_response.content:
-        return form_response
-
-    extra_context = {"form": form_response.content}
-    return object_list(request, Project.objects.all(),
-                       extra_context=extra_context,
-                       context_processors=["django.core.context_processors.media"])
-
-
-def project_detail(request, object_id):
-    """
-    Redirects users to either the current milestone for this project or the form
-    to add a new milestone.
-    """
-    project = get_object_or_404(Project, pk=object_id)
-    current_milestone = project.get_current_milestone()
-
-    if current_milestone:
-        return HttpResponseRedirect(current_milestone.get_absolute_url())
-    else:
-        return HttpResponseRedirect(reverse("projects_milestone_create",
-                                            args=(project.id,)))
-
-
-def milestone_detail(request, project_id, object_id=None):
-    """
-    Describes a milestone for a given project or a form to create a new
-    milestone for that same project.
-    """
-    project = get_object_or_404(Project, pk=project_id)
-
-    if object_id is None:
-        milestone = None
-        form_args = {}
-    else:
-        milestone = get_object_or_404(Milestone, pk=object_id)
-        form_args = {"instance": milestone}
-
-    if request.method == "POST":
-        form = MilestoneForm(request.POST, **form_args)
-        tasks_form = TasksForm(request.POST)
-        if form.is_valid():
-            milestone = form.save(commit=False)
-            milestone.project = project
-            milestone.save()
-            return HttpResponseRedirect(milestone.get_absolute_url())
-    else:
-        form = MilestoneForm(**form_args)
-        tasks_form = TasksForm()
-
-    context = {"form": form,
-               "tasks_form": tasks_form,
-               "project": project,
-               "object": milestone}
-    return render_to_response("projects/milestone_form.html", context,
-                              context_instance=RequestContext())
+    if form.is_valid():
+        task = form.save(commit=False)
+        task.reported_by = request.user
+        task.save()
+        task.assigned_to.add(request.user)
+        task.save_m2m()
+        form = TaskForm()
+    
+    context = {"page_title": "Projects",
+               "tasks": tasks,
+               "form": form}
+    return render_to_response("projects/index.html", context,
+                              context_instance=RequestContext(request))
