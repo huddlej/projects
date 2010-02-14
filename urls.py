263c38705ce037c0ff73297ad1a4175d3eb657ba
John Huddleston <huddlej@gmail.com>
2010-02-13 20:17:45 -0800

    replaced all other urls with a single index conf

--- urls.py
+++ urls.py
@@ -1,33 +1,8 @@
 from django.conf.urls.defaults import patterns, url
-from django.views.generic.create_update import create_object, update_object
-from django.views.generic.list_detail import object_detail
 
-from models import Milestone, Project, Task
-from views import milestone_detail, project_detail, projects
+from views import index
 
 
 urlpatterns = patterns("",
-    url(r"^$", projects, name="projects_index"),
-
-    url(r"^(?P<object_id>\d+)/$", project_detail,
-        name="projects_project"),
-
-    url(r"^add/$", create_object,
-        {"model": Project},
-        name="projects_project_add"),
-
-    url(r"^(?P<project_id>\d+)/milestone/(?P<object_id>\d+)/$",
-        milestone_detail,
-        name="projects_milestone"),
-
-    url(r"^(?P<project_id>\d+)/milestone/add/$", milestone_detail,
-        name="projects_milestone_create"),
-
-    url(r"^task/(?P<object_id>\d+)/$", update_object,
-        {"model": Task},
-        name="projects_task"),
-
-    url(r"^task/add/$", create_object,
-        {"model": Task},
-        name="projects_task_add"),
+    url(r"^$", index, name="projects_index"),
 )
