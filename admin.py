e78ff5baf47d73a25593ae53852c3ef1ac9b24b0
John Huddleston <huddlej@gmail.com>
2010-02-13 20:15:17 -0800

    removed references to milestone and project in admin.py

--- admin.py
+++ admin.py
@@ -1,32 +1,6 @@
 from django.contrib import admin
 
-from models import Milestone, Project, Task
+from models import Task
 
 
-class TaskInline(admin.StackedInline):
-    model = Task
-
-
-class MilestoneAdmin(admin.ModelAdmin):
-    inlines = [TaskInline]
-
-    def save_formset(self, request, form, formset, change):
-        instances = formset.save(commit=False)
-        for instance in instances:
-            instance.reported_by = request.user
-            instance.save()
-
-        formset.save_m2m()
-
-
-class MilestoneInline(admin.TabularInline):
-    model = Milestone
-
-
-class ProjectAdmin(admin.ModelAdmin):
-    inlines = [MilestoneInline]
-
-
-admin.site.register(Milestone, MilestoneAdmin)
-admin.site.register(Project, ProjectAdmin)
 admin.site.register(Task)
